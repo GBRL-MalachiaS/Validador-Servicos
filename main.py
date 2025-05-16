9c8906781a44174e979da58a6caba23eced71136
Gabriel Malachias Hangrad <607.gmalachias@rte-intra.com.br>
2025-05-16 16:37:29 -0300

    Envio de e-mail.
    Acrescentada as credencias, para envio de e-mail.

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import psutil
+import smtplib
+from google.oauth2.credentials import Credentials
 from datetime import datetime, timedelta
 
+
 servicos_windows = {}
+
 
 def validar_servico(nome_servico):
     for servico in psutil.win_service_iter():
@@ -18,11 +22,15 @@ def processamento(nome_servico):
             pid = servico.pid()
             processo = psutil.Process(pid)
             return {
-                "nome": processo.name(), #Metodo retorna o nome do processo
-                "uso_cpu": processo.cpu_percent(interval=1), #Metodo retornar o processamento de CPU
-                "uso_memoria": processo.memory_info().rss / (1024 ** 2),  # Retorna o uso de memoria em MB
-                "status": processo.status(), # Valida se o serviço está rodando. 
+                "nome": processo.name(),  # Metodo retorna o nome do processo
+                # Metodo retornar o processamento de CPU
+                "uso_cpu": processo.cpu_percent(interval=1),
+                # Retorna o uso de memoria em MB
+                "uso_memoria": processo.memory_info().rss / (1024 ** 2),
+                # Valida se o serviço está rodando.
+                "status": processo.status(),
             }
+
 
 def ultima_execucao(nome_servico):
     for servico in psutil.win_service_iter():
@@ -32,7 +40,7 @@ def ultima_execucao(nome_servico):
                 processo = psutil.Process(pid)
                 timestamp = processo.create_time()
                 hora_inicio = datetime.fromtimestamp(timestamp)
-                return hora_inicio #Retorna a dia e hora que foi a ultima execução do serviço
+                return hora_inicio  # Retorna a dia e hora que foi a ultima execução do serviço
             else:
                 return f"O serviço '{nome_servico}' está registrado, mas não possui um processo ativo."
     return f"O serviço '{nome_servico}' não foi encontrado."
@@ -40,12 +48,22 @@ def ultima_execucao(nome_servico):
 
 nome_do_servico = "WinDefend"
 hora_atual = datetime.now()
-
-print(validar_servico(nome_do_servico))
-print(processamento(nome_do_servico))
+# print(validar_servico(nome_do_servico))
+# print(processamento(nome_do_servico))
 execucao = ultima_execucao(nome_do_servico)
 
 tempo_sem_execucao = hora_atual - execucao
 
 if tempo_sem_execucao > timedelta(minutes=60):
+    creds = Credentials.from_authorized_user_file("token.json")
+
+    SMTP_SERVER = "smtp.gmail.com"
+    SMTP_PORT = 587
+
+    server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+    server.starttls()
+    server.login(creds.token, creds.token_uri)  # Autenticando via OAuth
+    server.sendmail("tecnologiarte607@gmail.com", "gabriel.malachias@rte.com.br", "Testando envio de e-mail via OAuth")
+    server.quit()
+
     print('é maior')
